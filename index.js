63780bf35f0403bc79b48b44b3b64f71c07d6ce1
Lennon Jesus <lennon.jesus@gmail.com>
2019-03-29 14:16:52 -0300

    refactor: Organizes code

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
 #!/bin/env node
 
-const WebSocketServer = require("ws").Server
-const http = require("http")
-const express = require("express")
-const app = express()
+const WebSocketServer = require("ws").Server;
+const http = require("http");
+const express = require("express");
+const app = express();
 
-const port        = process.env.OPENSHIFT_NODEJS_PORT || 5000;
+const port = process.env.OPENSHIFT_NODEJS_PORT || 5000;
 const isOpenShift = process.env.OPENSHIFT_NODEJS_PORT ? true : false;
 
-app.use(express.static(__dirname + "/"))
+app.use(express.static(__dirname + "/"));
 
-console.log(`Port: ${port} ${isOpenShift ? "OepnShift mode" : "Standalone mode"}`);
+console.log(
+  `Port: ${port} ${isOpenShift ? "OpenShif mode" : "Standalone mode"}`
+);
 
-const server = http.createServer(app)
-server.listen(port)
+const server = http.createServer(app);
+server.listen(port);
 
-console.log(`http server listening on ${port}`)
+console.log(`http server listening on ${port}`);
 
-const wss = new WebSocketServer({server: server})
+const wss = new WebSocketServer({ server: server });
 
-console.log("WebSocket Server Created")
+console.log("WebSocket Server Created");
 
 wss.on("connection", function(ws) {
   
-    const id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+  const id = setInterval(function() {
+    ws.send(JSON.stringify(new Date()), function() {});
+  }, 1000);
 
-  console.log("websocket connection open")
+  console.log("websocket connection open");
 
   ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
-})
+    console.log("websocket connection close");
+    clearInterval(id);
+  });
+});
